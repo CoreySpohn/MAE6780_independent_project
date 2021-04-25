066cedb62b53109fd4b3ac316bb69941335a542d
Rachel Oliver <75960310+ast-RO-engineer@users.noreply.github.com>
2021-04-25 17:29:42 -0400

    Debug FLIGHT MPC
    Small changes to the hurricane wind model and the main script to make it run. There is still one problem, after the simulation runs the velocity of the aircraft is equating to zero which then causes problems on the next iteration of the script.

--- plant/HurricaneEOM.m
+++ plant/HurricaneEOM.m
@@ -40,18 +40,18 @@ function z_hurr_dot = HurricaneEOM(t,z_hurricane_prev,hurr_para)
 v = hurr_para.linvel; v_th = hurr_para.angvel;
 
 % Unpack Previous Hurricane State
-z = z_hurricane_prev(1:3)';
+z = z_hurricane_prev(1:3)'; theta = z_hurricane_prev(3);
 
 % Create DCM to convert movement in hurricance fixed frame back to the
 % inertial one
 R_HI = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
 
 if v_th ==0
-    z_dot = R_HI*[v;v_th];
+    z_dot = R_HI*[v;0;v_th];
 else
     % Use definition of an arc to determine the components of change 
     z_dot = R_HI*[2*(v/v_th)*sin(v_th/2)*sin((pi-v_th)/2);2*(v/v_th)*sin(v_th/2)*cos((pi-v_th)/2);vth];
 end
     
-z_hurr_dot = z_dot';
+z_hurr_dot = z_dot;
 end
